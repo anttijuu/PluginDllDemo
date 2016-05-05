6f81beefcac7d1e49aedd0eb8e854dd353db0623
Antti Juustila <antti.juustila@gmail.com>
2016-05-06 00:48:26 +0300

    Fixed indents.

--- CryptoClientConsole/CryptoClientConsole/main.cpp
+++ CryptoClientConsole/CryptoClientConsole/main.cpp
@@ -15,7 +15,7 @@ void usingGoodLib();
 void usingBadLib();
 
 int main(int argc, const char * argv[]) {
-
+   
    bool userWantsIt = false;
    do {
       usingGoodLib();
@@ -29,58 +29,58 @@ int main(int argc, const char * argv[]) {
       }
    } while (userWantsIt);
    std::cout << "Thanks and bye! " << std::endl;
-	
-	return 0;
+   
+   return 0;
 }
 
 
 void usingGoodLib() {
    std::cout << std::endl << "Welcome to Good EasyCrypto(tm)!" << std::endl;
-	std::cout << "Enter text to encrypt: ";
-	
-	std::string userString;
-	std::string result;
-	std::getline(std::cin, userString);
-	
-	EasyCrypto::encrypt(userString, result, EasyCrypto::Method::Reverse);
-	std::cout << "Text encrypted with Reverse method: ";
-	std::cout << result << std::endl;
-	std::cout << "Decrypting back to original: ";
-	EasyCrypto::decrypt(result, userString, EasyCrypto::Method::Reverse);
-	std::cout << userString << std::endl << std::endl;
-	
-	EasyCrypto::encrypt(userString, result, EasyCrypto::Method::Matrix);
-	std::cout << "Text encrypted with Matrix method: ";
-	std::cout << result << std::endl;
-	std::cout << "Decrypting back to original: ";
-	EasyCrypto::decrypt(result, userString, EasyCrypto::Method::Matrix);
+   std::cout << "Enter text to encrypt: ";
+   
+   std::string userString;
+   std::string result;
+   std::getline(std::cin, userString);
+   
+   EasyCrypto::encrypt(userString, result, EasyCrypto::Method::Reverse);
+   std::cout << "Text encrypted with Reverse method: ";
+   std::cout << result << std::endl;
+   std::cout << "Decrypting back to original: ";
+   EasyCrypto::decrypt(result, userString, EasyCrypto::Method::Reverse);
    std::cout << userString << std::endl << std::endl;
-	
+   
+   EasyCrypto::encrypt(userString, result, EasyCrypto::Method::Matrix);
+   std::cout << "Text encrypted with Matrix method: ";
+   std::cout << result << std::endl;
+   std::cout << "Decrypting back to original: ";
+   EasyCrypto::decrypt(result, userString, EasyCrypto::Method::Matrix);
+   std::cout << userString << std::endl << std::endl;
+   
 }
 
 void usingBadLib() {
    std::cout << "==============================" << std::endl;
    std::cout << "Welcome to Bad EasyCrypto(tm)!" << std::endl;
-	std::cout << "Enter text to encrypt: ";
-	
-	std::string userString;
-	std::string result;
-	std::getline(std::cin, userString);
-	
+   std::cout << "Enter text to encrypt: ";
+   
+   std::string userString;
+   std::string result;
+   std::getline(std::cin, userString);
+   
    std::unique_ptr<EasyCryptoLibBad> crypter(new EasyCryptoLibBad());
-	
-	crypter->encrypt(userString, result, EasyCryptoLibBad::Method::Reverse);
-	std::cout << "Text encrypted with Reverse method: ";
-	std::cout << result << std::endl;
-	std::cout << "Decrypting back to original: ";
-	crypter->decrypt(result, userString, EasyCryptoLibBad::Method::Reverse);
-	std::cout << userString << std::endl << std::endl;
-	
-	crypter->encrypt(userString, result, EasyCryptoLibBad::Method::Matrix);
-	std::cout << "Text encrypted with Matrix method: ";
-	std::cout << result << std::endl;
-	std::cout << "Decrypting back to original: ";
-	crypter->decrypt(result, userString, EasyCryptoLibBad::Method::Matrix);
-	std::cout << userString << std::endl << std::endl;
-		
+   
+   crypter->encrypt(userString, result, EasyCryptoLibBad::Method::Reverse);
+   std::cout << "Text encrypted with Reverse method: ";
+   std::cout << result << std::endl;
+   std::cout << "Decrypting back to original: ";
+   crypter->decrypt(result, userString, EasyCryptoLibBad::Method::Reverse);
+   std::cout << userString << std::endl << std::endl;
+   
+   crypter->encrypt(userString, result, EasyCryptoLibBad::Method::Matrix);
+   std::cout << "Text encrypted with Matrix method: ";
+   std::cout << result << std::endl;
+   std::cout << "Decrypting back to original: ";
+   crypter->decrypt(result, userString, EasyCryptoLibBad::Method::Matrix);
+   std::cout << userString << std::endl << std::endl;
+   
 }
