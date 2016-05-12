57786413b49586a9512f38eb87f472ccacb8ab53
Antti Juustila <antti.juustila@gmail.com>
2016-05-12 19:39:22 +0300

    Included new files for template classes

--- EasyCryptoLib/EasyCryptoLib/EasyCryptoLibBad.hpp
+++ EasyCryptoLib/EasyCryptoLib/EasyCryptoLibBad.hpp
@@ -12,8 +12,8 @@
 #include <string>
 #include <memory>
 
-class EasyCryptoPrivReverse;
-class EasyCryptoPrivMatrix;
+#include "EasyCryptoPrivReverse.hpp"
+#include "EasyCryptoPrivMatrix.hpp"
 
 /* The classes below are exported */
 #pragma GCC visibility push(default)
