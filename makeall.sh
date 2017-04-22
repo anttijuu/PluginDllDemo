8ce0b3cc89f2253a740b99d61ba5a7afa36f2398
Antti Juustila <antti.juustila@oulu.fi>
2017-04-22 14:58:06 +0300

    Added makeall.sh

---
+++ makeall.sh
@@ -0,0 +1,18 @@
+cd EasyCryptoLib
+cmake .
+make
+sudo make install
+cd ..
+cd CryptoClientConsole
+cmake .
+make
+cd ..
+cd EasyCryptoServer
+cmake .
+make
+cd ..
+cd EasyCryptoClient
+cmake .
+make
+cd ..
+
